46456a6792d1470976be23eff3b4cf7aacc8c842
Linton Junior <lintonjr@hotmail.com>
2023-12-04 19:02:58 -0400

    first commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './heroes/mocks/Hero.mocks';
+import { Hero } from './Hero';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService){}
+  getHeroesMock():Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add("Hero Service: fetched heroes");
+    return heroes
+  }
+  getHero(id: number):Observable<Hero> {
+    const hero = HEROES.find(heroValue => heroValue.id === id)!;
+    this.messageService.add(`HeroService: Fetched hero id=${id}`)
+    return of(hero)
+  }
+}
